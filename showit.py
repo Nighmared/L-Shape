3d5f0c8b7656e10b8dc6f00e7f93b6c0ec03fe78
nighmared <konrathj@student.ethz.ch>
2019-11-12 15:47:24 +0100

    builup

--- showit.py
+++ showit.py
@@ -1,19 +1,26 @@
 import matplotlib.pyplot as plt
-import matplotlib.animation as anim
+import matplotlib.animation as animation
+import imageio
 #import numpy as np
 #a = np.random.random((16,16))
+otherstates = []
 CMAP = "viridis"
-
+counter = 0
 class statelist():
 	def __init__(self):
 		self.states = []
+		self.otherstates =[]
 
 	def addstate(self,state):
-		global CMAP
-		self.states.append([plt.imshow(state,cmap=CMAP,interpolation="nearest",animated=True)])
+		global counter
+		nexto = [plt.imshow(state,cmap=CMAP,interpolation="nearest",animated=True)]
+		fname = f"{counter}"
+		#plt.savefig(fname,nexto[0])
+		counter+=1
+		self.states.append(nexto)
+
 
 def animo(statelist):
 	fig = plt.figure()
-	an = anim.ArtistAnimation(fig,statelist,interval=200,blit=True,repeat=False)
-	#ani.save('l-shape.mp4')
-	plt.show()
+	anim = animation.ArtistAnimation(fig,statelist,interval=200,blit=True,repeat=False)
+	plt.show()	
